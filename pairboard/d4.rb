b0a6e91bc7b362db1c8aa04b2c43900c1a1e0e58
Vincent Chan <vincent.chan@biola.edu>
2017-08-03 13:26:05 -0700

    largest contiguous sum

--- pairboard/d4.rb
+++ pairboard/d4.rb
@@ -9,3 +9,24 @@ def uniq_subs(str)
   end
   set_store
 end
+
+def largest_contiguous_sum(arr)
+  largest_sum = 0
+  running_total = 0
+
+  arr.each do |num|
+    if running_total + num > 0
+      running_total += num
+      largest_sum = running_total if running_total > largest_sum
+    else
+      running_total = 0
+    end
+  end
+
+  if largest_sum > 0
+    return largest_sum
+  else
+    return arr.max
+  end
+
+end
