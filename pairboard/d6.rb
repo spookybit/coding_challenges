b36acd8bc15fdb133877036945d5ae2d1ded24d5
Vincent Chan <vincent.chan@biola.edu>
2017-08-03 21:37:50 -0700

    intersection

--- pairboard/d6.rb
+++ pairboard/d6.rb
@@ -7,3 +7,16 @@ def subsets(arr)
   subs = subsets(rest)
   subs + subs.map {|sub| sub + [first]}
 end
+
+require 'set'
+
+def fast_intersection(arr1, arr2)
+  arr_set = arr1.to_set
+  intersections = []
+
+  arr2.each do |num|
+    intersections << num if arr_set.include?(num)
+  end
+
+  intersections
+end
