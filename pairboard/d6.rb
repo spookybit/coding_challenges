5ce6ef820325672b777fb1b532fcc956adce446c
Vincent Chan <vincent.chan@biola.edu>
2017-08-03 22:20:51 -0700

    can find zero

--- pairboard/d6.rb
+++ pairboard/d6.rb
@@ -24,3 +24,25 @@ end
 def common_subsets(arr1, arr2)
   subsets(fast_intersection(arr1, arr2))
 end
+
+def can_find_zero(arr, idx)
+  to_visit = [idx]
+  visited = Array.new(arr.length, false)
+  visited[idx] = true
+
+  until to_visit.empty?
+    indx = to_visit.shift
+    value = arr[indx]
+    p indx
+    p value
+    return true if value == 0
+
+    visited[indx] = true
+
+    to_visit << arr[indx + value - 1] unless visited[indx + value - 1] || (indx + value) > visited.length
+    to_visit << arr[indx - value] unless visited[indx - value] || (indx - value) < 0
+
+  end
+
+  false
+end
