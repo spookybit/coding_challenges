9ff95dbd350c9f9f2408c9101c7910a58f00610d
Vincent Chan <vincent.chan@biola.edu>
2017-09-29 17:10:39 -0700

    d7

--- pairboard/d7.rb
+++ pairboard/d7.rb
@@ -1,1 +1,30 @@
+# Noncomparison Sort
+#
+# Part 1: Say that I gave you an array of length n, containing the numbers 1..n in jumbled order. "Sort" this array in O(n) time. You should be able to do this without looking at the input.
 
+def sort1(arr)
+  (1..arr.length).to_a
+end
+
+# Part 2: Say that I give you an array of length n with numbers in the range 1..N (N >= n). Sort this array in O(n + N) time. You may use O(N) memory.
+
+def sort2(arr, max)
+  arr.sort
+end
+
+# Part 3: Say I give you an array of n strings, each of length k. I claim that, using merge sort, you can sort this in O(knlog(n)), since comparing a pair of strings takes O(k) time.
+#
+# I want you to beat that. Sort the strings in O(kn). Hint: do not compare any two strings. You may assume all strings contain only lowercase letters a..z without whitespace or punctuation.
+
+# Given an array, write a function that will return a random index of the array. The probability of an index being returned is weighted by the value at that index against the sum of the array values. For example, for the array [4, 6, 8], index 0 should be returned with 4 in 18 odds, index 1 should be returned with 6 in 18 odds, and index 2 should be return with 8 in 18 odds. Implement this in O(n) time.
+
+def weighted_random_index(arr)
+  total = arr.inject(:+)
+  goal = rand(total)
+
+  count = 0
+  arr.each_index do |idx|
+    count += arr[idx]
+    return idx if count > goal
+  end
+end
