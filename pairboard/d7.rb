e28c17e53fde046cb09cea90b1d4b731a478c90a
Vincent Chan <vincent.chan@biola.edu>
2017-10-01 00:03:11 -0700

    look and say d7

--- pairboard/d7.rb
+++ pairboard/d7.rb
@@ -50,3 +50,44 @@ def move_zeros(arr)
 
   arr
 end
+
+#
+# Implement the 'look and say' function. 'Look and say' takes an input array and outputs an array that describes the count of the elements in the input array as they appear in order.
+#
+# Example:
+#
+# # there is one '1' in the input array
+# look_and_say([1]) == [[1, 1]]
+#
+# # there are two '1's in the input array
+# look_and_say([1, 1]) == [[2, 1]]
+#
+# # there is one '2', followed by one '1' in the input array
+# look_and_say([2, 1]) == [[1, 2], [1, 1]]
+#
+# # is one '1', followed by one '2', followed by 2 '1's in the input
+# # array
+# look_and_say([1, 2, 1, 1]) == [[1, 1], [1, 2], [2, 1]]
+
+def look_and_say(arr)
+  result = []
+  current = arr[0]
+  count = 0
+
+  arr.each_with_index do |num, idx|
+    if current == num
+      count += 1
+      if result.empty?
+        result << [count, current]
+      else
+        result[-1] = [count, current]
+      end
+    else
+      count = 1
+      result << [count, num]
+      current = arr[idx]
+    end
+  end
+
+  result
+end
