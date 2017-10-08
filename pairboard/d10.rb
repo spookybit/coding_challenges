fc085218e90d9fa6c1c77f5abb8c41e1252f99f6
Vincent Chan <vincent.chan@biola.edu>
2017-10-07 22:21:11 -0700

    factorial d10

--- pairboard/d10.rb
+++ pairboard/d10.rb
@@ -10,3 +10,17 @@ def binary(num)
 
   result.empty? ? "0" : result.to_i
 end
+
+# Implement factorial with and without recursion. What is a potential disadvantage of the recursive way?
+#
+# What is tail-recursion? Does Ruby have tail-call optimization? Pretend it did; write a tail-recursive version of rec_fac.
+
+def factorial(num)
+  total = 1
+  until num == 0
+    total *= num
+    num -= 1
+  end
+
+  total
+end
