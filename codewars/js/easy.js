288b160940db07f6d18ccb0df9f07e039f586d1a
Vincent Chan <vincent.chan@biola.edu>
2017-11-20 22:32:39 -0800

    easy code challenges

--- codewars/js/easy.js
+++ codewars/js/easy.js
@@ -27,3 +27,48 @@ function tribonacci(signature,n){
   }
   return signature;
 }
+
+//find parity outlier; returns the only even or odd number of an array of at least 3 numbers
+function findOutlier(integers){
+  //your code here
+  let evenCount = 0;
+  let oddCount = 0;
+  let evenNum, oddNum;
+  for (let num of integers) {
+    if (num % 2 === 0) {
+      evenCount += 1;
+      evenNum = num;
+
+      if (evenCount>= 1 && oddCount >= 1 && evenCount + oddCount >= 3) {
+        if (evenCount > oddCount) {return oddNum}
+        else {return evenNum}
+      }
+    } else {
+      oddCount += 1;
+      oddNum = num;
+      if (evenCount>= 1 && oddCount >= 1 && evenCount + oddCount >= 3) {
+        if (evenCount > oddCount) {return oddNum}
+        else {return evenNum}
+      }
+    }
+  }
+}
+
+// function findOutlier(int){
+//   var even = int.filter(a=>a%2==0);
+//   var odd = int.filter(a=>a%2!==0);
+//   return even.length==1? even[0] : odd[0];
+// }
+
+function isTriangle(a,b,c)
+{
+  let arr = [a,b,c]
+  // let max = Math.max(...arr);
+  // let index = arr.indexOf(max);
+  // arr.splice(index, 1);
+
+  arr.sort(function(x,y) {x-y});
+
+  if (arr[0] + arr[1] > arr[2]) {return true}
+  else {return false}
+}
