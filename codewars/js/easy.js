0194251c20badfdad8717b120e7c795d13d02eab
Vincent Chan <vincent.chan@biola.edu>
2017-11-26 23:48:14 -0800

    findEvenIndex

--- codewars/js/easy.js
+++ codewars/js/easy.js
@@ -93,3 +93,29 @@ function addDigits(num) {
   }
   return total;
 }
+
+//You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+function findEvenIndex(arr)
+{
+  for (let i = 0, len = arr.length-1; i < len; i++) {
+    let leftArr = arr.slice(0,i);
+    let rightArr = arr.slice(i+1);
+
+    let left = leftArr[0] ? leftArr.reduce((sum, num) =>sum + num) : 0;
+    let right = rightArr[0] ? rightArr.reduce((sum, num) =>sum + num) : 0;
+
+    if (left === right) {return i}
+  }
+  return -1
+}
+
+// function findEvenIndex(arr)
+// {
+//   for(var i=1; i<arr.length-1; i++) {
+//     if(arr.slice(0, i).reduce((a, b) =>  a+b) === arr.slice(i+1).reduce((a, b) =>  a+b)) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
