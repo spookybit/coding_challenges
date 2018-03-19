7551034d0594f8ffeefcd848ad88699bb1ad39c6
Vincent Chan <vincent.chan@biola.edu>
2018-03-19 13:29:59 -0700

    style structure

--- frontend/expediaDeals/script.js
+++ frontend/expediaDeals/script.js
@@ -2,7 +2,10 @@ const flightItems = document.querySelectorAll('.flightItem');
 
 flightItems.forEach(function(item){
   let newEl = document.createElement('div');
-  newEl.innerHTML = 'ROUNDTRIP FROM'
+  let newElText = document.createElement('span');
+  newElText.innerHTML = 'ROUNDTRIP FROM';
+  newEl.appendChild(newElText);
+  newEl.className = 'tripDescription';
   let priceEl = item.querySelector('.price');
   item.insertBefore(newEl, priceEl);
 })
